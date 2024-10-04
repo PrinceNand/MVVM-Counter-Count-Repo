bbf0ab482e525b29f2191d6c0e14d837f38abb56
Prince <ashnand2000@gmail.com>
2024-10-04 21:34:38 +0530

    MVVM_COUNTER: 2.Creating layout and functionality for simple counter

--- app/src/main/java/com/example/rotationrepomvvmkotlin/MainActivity.kt
+++ app/src/main/java/com/example/rotationrepomvvmkotlin/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.rotationrepomvvmkotlin.ui.theme.RotationRepoMVVMKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +31,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RotationRepoMVVMKotlinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Developers",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TheCounterApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RotationRepoMVVMKotlinTheme {
-        Greeting("Android")
+fun TheCounterApp() {
+    val count = remember { mutableStateOf(0) }
+
+    fun increment() {
+        count.value++
+    }
+
+    fun decrement() {
+        count.value--
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Counter: ${count.value}",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+            )
+        Row {
+            Button(onClick = {increment()}) {
+                Text(text = "Add +")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = {decrement()}) {
+                Text(text = "Sub -")
+            }
+        }
     }
 }
