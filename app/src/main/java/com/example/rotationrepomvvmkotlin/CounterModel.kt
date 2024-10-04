30716dcf5b13a1b589625b44b1ab37c4e32c42f9
Prince <ashnand2000@gmail.com>
2024-10-04 22:13:09 +0530

    MVVM_COUNTER: 5.Creating Data Class and Counter Model Repo to internally handle View and ViewModel

---
+++ app/src/main/java/com/example/rotationrepomvvmkotlin/CounterModel.kt
@@ -0,0 +1,18 @@
+package com.example.rotationrepomvvmkotlin
+
+data class CounterModel(var count: Int)
+
+class CounterRepository {
+    private  var counter = CounterModel(0)
+
+    fun getCounter() = counter
+
+    fun incrementCounter(){
+        counter.count++
+    }
+
+    fun decrementCounter(){
+        counter.count--
+    }
+
+}
--- app/src/main/java/com/example/rotationrepomvvmkotlin/CounterViewModel.kt
+++ app/src/main/java/com/example/rotationrepomvvmkotlin/CounterViewModel.kt
@@ -4,19 +4,28 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class CounterViewModel: ViewModel() {
+class CounterViewModel(): ViewModel() {
+
+    private val _repository: CounterRepository = CounterRepository()
 
     // private variable for this class
-    private val _count = mutableStateOf(0)
+    private val _count = mutableStateOf(_repository.getCounter().count)
 
     // public variable to access outside this class
     val count: MutableState<Int> = _count
 
     fun increment() {
-        _count.value++
+        // increment using model
+        _repository.incrementCounter()
+        // add value to the private variable to send to local variable
+        _count.value = _repository.getCounter().count
     }
 
     fun decrement() {
-        _count.value--
+        // decrement using model
+        _repository.decrementCounter()
+        // add value to the private variable to send to local variable
+        _count.value = _repository.getCounter().count
+
     }
 }
